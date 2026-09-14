e2d9555cd3462f2d0e2a821fb87e76d8b3e63328
Yu Yu <yuyu22@sjtu.edu.cn>
2026-09-14 10:58:01 +0800

    set hyperuniform main program ass a function

---
+++ example/test_hyperuniform_tiling.py
@@ -0,0 +1,8 @@
+
+
+from just_tiling.hyperuniform.hyperuniform_tiling import hyperuniform_tiling
+
+inputcatalog = "/home/Survey/JUST/mini/junyumock/lightcone_ra_0_90_dec_0_90_rmagcut20.5_cluster_mask.fits"
+outputdir = "./output/" 
+
+hyperuniform_tiling(inputcatalog, outputdir)
